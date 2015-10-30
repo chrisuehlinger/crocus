fb02a094957f87f3255884e5e46f13e52ed0ec3d
Chris Uehlinger <chris.uehlinger@gmail.com>
2015-10-30 17:43:35 -0400

    Fixed landing page bugs

--- public/js/landingpage.js
+++ public/js/landingpage.js
@@ -1,3 +1,4 @@
-d3.json("/public/data/sample-directory.json", function (error, root) {
+d3.json("public/data/sample-directory.json", function (error, root) {
+    if (error) throw error;
     render(root);
 });
