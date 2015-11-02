15642c9fe783dc1b90d6b6c7d0430bf073958cff
Chris Uehlinger <chris.uehlinger@gmail.com>
2015-11-02 16:10:59 -0500

    Created depth limited tree API

--- routes/directory.js
+++ routes/directory.js
@@ -61,6 +61,67 @@ router.get('/tree', function (req, res) {
 
 });
 
+/* GET directory tree listing. */
+router.get('/lazy', function (req, res) {
+    var root = req.query.root;
+    var maxDepth = +req.query.maxdepth;
+
+    var traverseFileSystem = function (currentPath, currentDepth) {
+        return fs.list(currentPath)
+            .then(function (files) {
+                var promises = [];
+                files.forEach(function (file) {
+                    var currentFile = currentPath + path.sep + file;
+                    var promise = fs.stat(currentFile)
+                        .then(function (stat) {
+                            if (stat.isFile()) {
+                                return {
+                                    name: file,
+                                    fullPath: currentFile,
+                                    size: stat.size
+                                };
+                            } else if (stat.isDirectory()) {
+                                if(currentDepth < maxDepth){
+                                    return traverseFileSystem(currentFile, currentDepth+1).then(function (children){
+                                        return {
+                                            name: file,
+                                            fullPath: currentFile,
+                                            size: stat.size,
+                                            children: children
+                                        };
+                                    });
+                                } else {
+                                    return {
+                                        name: file,
+                                        fullPath: currentFile,
+                                        size: stat.size
+                                    };
+                                }
+                            }
+                        });
+                    promises.push(promise);
+                });
+                return q.all(promises);
+            });
+    };
+    traverseFileSystem(root, 0).then(function (children) {
+        var totalSize = 0;
+        children.forEach(function (child) {
+            totalSize += child.size;
+        });
+
+        var tree = {
+            name: root,
+            fullPath: root,
+            size: totalSize,
+            children: children
+        };
+//        res.send(tree);
+                res.send('<pre><code>' + JSON.stringify(tree, null, '  ') + '</code></pre>');
+    });
+
+});
+
 router.get('/autocomplete', function (req, res) {
     var root = req.query.root;
     var basename = '';
